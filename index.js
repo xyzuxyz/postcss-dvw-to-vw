37a32e9e456bf3b9c9497abc34b3167195022317
kosku <kosgug@gmx.com>
2023-08-09 11:27:14 +0800

    Update type is module

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 // postcss-dvw-to-vw.js
 // version: 1.0.0
-export default (width=750) => {
+export default (option={}) => {
+
+    // Set default
+    option.width = option.width ?? 750
+    option.unit = option.unit ?? "dvw"
+
+    // Convert
     return {
         postcssPlugin: 'postcss-dvw-to-vw',
         prepare (result) {
             return {
                 Declaration (node) {
-                    const match =node.value.match(/\d+(\.\d+)?dvw/g)
+                    const match =node.value.match(/\d+(\.\d+)?option.unit/g)
                     if(match) {
                         for(let i = 0; i < match.length; i++) {
                             node.value = node.value.replace(
                                 match[i],
-                                (match[i].replace("dvw", "")*100/width)+"vw"
+                                (match[i].replace(option.unit, "")*100/option.width)+"vw"
                             );
                         }
                     }
---
+++ test.js
@@ -0,0 +1,20 @@
+import postcssDvwToVw from "./index.js";
+
+
+postcssDvwToVw()
+
+postcssDvwToVw({})
+
+postcssDvwToVw({
+    width: 375
+})
+
+postcssDvwToVw({
+    width: 375,
+    unit: "dvw"
+})
+
+postcssDvwToVw({
+    width: 750,
+    unit: "rpx"
+})
